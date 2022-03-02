f17e6bf881d02ca0113ea78b683e26a720d325ea
Esteban5957L <4estebanvillalba20@gmail.com>
2022-03-02 12:24:05 -0300

    avance

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
-import data, { Cairns } from './data.js';
+
+const APIKEY = process.env.REACT_APP_WHEATER_API_KEY;
 
 function App() {
+
+  const [cities,setCities] = useState([])
+
+  function handleRemoveCity(cityId){
+    setCities((prevCities)=>{
+      return prevCities.filter(
+        (city)=>{return city.id !== cityId}
+      )
+    });
+  }
+  
+  function onSearch(ciudad){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${APIKEY}&units=metric`)
+    .then((respuesta)=>respuesta.json())
+    .then((resource)=>{
+      if(resource.main !== undefined){
+        const city = {
+          min: Math.round(resource.main.temp_min),
+          max: Math.round(resource.main.temp_max),
+          img: resource.weather[0].icon,
+          id: resource.id,
+          wind: resource.wind.speed,
+          temp: resource.main.temp,
+          name: resource.name,
+          weather: resource.weather[0].main,
+          clouds: resource.clouds.all,
+          latitude: resource.coord.lat,
+          longitud: resource.coord.lon
+        };
+        setCities(oldCities => [...oldCities, city]);
+        } else {alert('City not found');}
+      });
+  }
+
   return (
     <div className="App">
       <div>
-        <Card
-          max={Cairns.main.temp_max}
-          min={Cairns.main.temp_min}
-          name={Cairns.name}
-          img={Cairns.weather[0].icon}
-          onClose={() => alert(Cairns.name)}
-        />
+        <SearchBar onSearch={onSearch}/>
       </div>
+
       <hr />
+
       <div>
         <Cards
-          cities={data}
+          cities={cities} onRemove={handleRemoveCity}
         />
       </div>
+      
       <hr />
-      <div>
-        <SearchBar
-          onSearch={(ciudad) => alert(ciudad)}
-        />
-      </div>
     </div>
   );
 }
